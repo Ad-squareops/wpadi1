304013811215edd7f0526a0675fc6538b31c5b89
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2022-12-16 20:20:46 +0530

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "s1" {
+  ami                    = "ami-0ada6d94f396377f2"
+  instance_type          = "t3a.small"
+  availability_zone      = "us-east-2a"
+  key_name               = "adikp1"
+  security_groups        = [aws_security_group.SG.id]
+  subnet_id              = element(module.vpc.public_subnets, 0)
+  vpc_security_group_ids = [aws_security_group.SG.id]
+
+  tags = {
+    Name = "adi-test-public"
+  }
+}
