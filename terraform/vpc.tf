b57269720688ee4b2a506c360eccd60bbf4b0a7c
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2022-12-16 20:20:01 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,22 @@
+
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name                 = "testVPCAdi"
+  cidr                 = "20.0.0.0/16"
+  azs                  = ["us-east-2a", "us-east-2b"]
+  private_subnets      = [cidrsubnet("20.0.0.0/16", 8, 0), cidrsubnet("20.0.0.0/16", 8, 1)]
+  public_subnets       = [cidrsubnet("20.0.0.0/16", 8, 2), cidrsubnet("20.0.0.0/16", 8, 3)]
+  private_subnet_names = ["Adi-Private-Subnet-One", "Adi-Private-Subnet-Two"]
+  public_subnet_names  = ["Adi-Public-Subnet-One", "Adi-Public-Subnet-Two"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  vpc_tags = {
+    Name      = "testVPCAdi"
+    terraform = "true"
+  }
+}
