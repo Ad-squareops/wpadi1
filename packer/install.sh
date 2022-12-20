94131dd5561c874f0a8db4f47ff79621e74c1775
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2022-12-20 11:24:44 +0530

    Update install.sh

--- packer/install.sh
+++ packer/install.sh
@@ -9,7 +9,7 @@ cd wordpress/
 sudo cp -r * /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/
 cd /etc/nginx/sites-enabled/
-sudo wget https://github.com/aarshsqaureops/nginx-configuration/archive/refs/heads/main.zip
+sudo wget https://github.com/Ad-squareops/Adi/archive/refs/heads/main.zip
 sudo unzip main.zip
 sudo mv nginx-configuration-main/wordpress /etc/nginx/sites-enabled/
 sudo systemctl reload php7.4-fpm.service
