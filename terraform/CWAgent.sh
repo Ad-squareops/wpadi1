3190a570b2dd3feeb44602f792a6d0bac6889872
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2022-12-22 12:54:12 +0530

    Update CWAgent.sh

--- terraform/CWAgent.sh
+++ terraform/CWAgent.sh
@@ -6,95 +6,94 @@ sudo touch amazon-cloudwatch-agent.json
 sudo chmod +rwx amazon-cloudwatch-agent.json
 tee -a amazon-cloudwatch-agent.json << END 
 {
-        "agent": {
-                "metrics_collection_interval": 30,
-                "run_as_user": "root"
-        },
-        "logs": {
-                "logs_collected": {
-                        "files": {
-                                "collect_list": [
-                                        {
-                                                "file_path": "/var/log/nginx/access.log",
-                                                "log_group_name": "Adi-HTTP-Access-Log",
-                                                "log_stream_name": "{instance_id}",
-                                                "retention_in_days": -1
-                                        },
-                                        {
-                                                "file_path": "/var/log/nginx/error.log",
-                                                "log_group_name": "Adi-HTTP-Error-Log",
-                                                "log_stream_name": "{instance_id}",
-                                                "retention_in_days": -1
-                                        }
-
-                                ]
-                        }
-                }
-        },
-        "metrics": {
-                "aggregation_dimensions": [
-                        [
-                                "InstanceId"
-                        ]
-                ],
-                "append_dimensions": {
-                        "AutoScalingGroupName": "${aws:AutoScalingGroupName}",
-                        "ImageId": "${aws:ImageId}",
-                        "InstanceId": "${aws:InstanceId}",
-                        "InstanceType": "${aws:InstanceType}"
-                },
-                "metrics_collected": {
-                        "cpu": {
-                                "measurement": [
-                                        "cpu_usage_idle",
-                                        "cpu_usage_iowait",
-                                        "cpu_usage_user",
-                                        "cpu_usage_system"
-                                ],
-                                "metrics_collection_interval": 30,
-                                "resources": [
-                                        "*"
-                                ],
-                                "totalcpu": false
-                        },
-                        "disk": {
-                                "measurement": [
-                                        "used_percent",
-                                        "inodes_free"
-                                ],
-                                "metrics_collection_interval": 30,
-                                "resources": [
-                                        "*"
-                                ]
-                        },
-                        "diskio": {
-                                "measurement": [
-                                        "io_time"
-                                ],
-                                "metrics_collection_interval": 30,
-                                "resources": [
-                                        "*"
-                                ]
-                        },
-                        "mem": {
-                                "measurement": [
-                                        "mem_used_percent"
-                                ],
-                                "metrics_collection_interval": 30
-                        },
-                        "statsd": {
-                                "metrics_aggregation_interval": 10,
-                                "metrics_collection_interval": 30,
-                                "service_address": ":8125"
-                        },
-                        "swap": {
-                                "measurement": [
-                                        "swap_used_percent"
-                                ],
-                                "metrics_collection_interval": 30
-                        }
-                }
-        }
+  "agent": {
+    "metrics_collection_interval": 30,
+    "run_as_user": "root"
+  },
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/nginx/access.log",
+            "log_group_name": "Adi-HTTP-Access-Log",
+            "log_stream_name": "{instance_id}",
+            "retention_in_days": -1
+          },
+          {
+            "file_path": "/var/log/nginx/error.log",
+            "log_group_name": "Adi-HTTP-Error-Log",
+            "log_stream_name": "{instance_id}",
+            "retention_in_days": -1
+          }
+        ]
+      }
+    }
+  },
+  "metrics": {
+    "aggregation_dimensions": [
+      [
+        "InstanceId"
+      ]
+    ],
+    "append_dimensions": {
+      "AutoScalingGroupName": "${aws:AutoScalingGroupName}",
+      "ImageId": "${aws:ImageId}",
+      "InstanceId": "${aws:InstanceId}",
+      "InstanceType": "${aws:InstanceType}"
+    },
+    "metrics_collected": {
+      "cpu": {
+        "measurement": [
+          "cpu_usage_idle",
+          "cpu_usage_iowait",
+          "cpu_usage_user",
+          "cpu_usage_system"
+        ],
+        "metrics_collection_interval": 30,
+        "resources": [
+          "*"
+        ],
+        "totalcpu": false
+      },
+      "disk": {
+        "measurement": [
+          "used_percent",
+          "inodes_free"
+        ],
+        "metrics_collection_interval": 30,
+        "resources": [
+          "*"
+        ]
+      },
+      "diskio": {
+        "measurement": [
+          "io_time"
+        ],
+        "metrics_collection_interval": 30,
+        "resources": [
+          "*"
+        ]
+      },
+      "mem": {
+        "measurement": [
+          "mem_used_percent"
+        ],
+        "metrics_collection_interval": 30
+      },
+      "statsd": {
+        "metrics_aggregation_interval": 10,
+        "metrics_collection_interval": 30,
+        "service_address": ":8125"
+      },
+      "swap": {
+        "measurement": [
+          "swap_used_percent"
+        ],
+        "metrics_collection_interval": 30
+      }
+    }
+  }
 }
 END
 
