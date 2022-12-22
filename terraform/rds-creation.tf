5d573b267d03c34e058de71d3693a76fc25889dc
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2022-12-22 17:09:57 +0530

    Update rds-creation.tf

--- terraform/rds-creation.tf
+++ terraform/rds-creation.tf
@@ -1,3 +1,12 @@
+resource "aws_db_subnet_group" "dbsub" {
+  name       = "adi-subnet-group-rds"
+  subnet_ids = ["subnet-0137f79e0f79ac457", "subnet-0f538e3830ee89ae2"]
+
+  tags = {
+    Name = "Adi DB subnet group"
+  }
+}
+
 resource "aws_db_instance" "rds" {
   allocated_storage      = 20
   db_name                = "mydb"
@@ -8,16 +17,15 @@ resource "aws_db_instance" "rds" {
   instance_class         = "db.t3.micro"
   username               = "admin"
   password               = "password"
-  db_subnet_group_name   = "terraform-rds-adi-20221222092936693600000001"
   parameter_group_name   = "default.mysql8.0"
   skip_final_snapshot    = true
   port                   = 3306
   publicly_accessible    = true
   vpc_security_group_ids = ["sg-0b05b41c832f5a2f3"]
+  db_subnet_group_name   = aws_db_subnet_group.dbsub.id
   storage_type           = "gp2"
 
   tags = {
     Name = "testdb"
   }
 }
-
