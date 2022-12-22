06d9b995a30b414f98fb2d3785f4f98b9d9fbae4
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2022-12-22 12:10:42 +0530

    Create CWAgent.sh

---
+++ terraform/CWAgent.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo wget https://s3.us-east-2.amazonaws.com/amazoncloudwatch-agent-us-east-2/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i amazon-cloudwatch-agent.deb
+sudo touch amazon-cloudwatch-agent.json
+sudo chmod +rwx amazon-cloudwatch-agent.json
+tee -a amazon-cloudwatch-agent.json << END 
+{
+	"agent": {
+		"metrics_collection_interval": 30,
+		"run_as_user": "root"
+	},
+	"logs": {
+		"logs_collected": {
+			"files": {
+				"collect_list": [
+					{
+						"file_path": "/var/log/nginx/access.log",
+						"log_group_name": "Adi-HTTP-Access-Log",
+						"log_stream_name": "{instance_id}",
+						"retention_in_days": -1
+					},
+                                        {
+						"file_path": "/var/log/nginx/error.log",
+						"log_group_name": "Adi-HTTP-Error-Log",
+						"log_stream_name": "{instance_id}",
+						"retention_in_days": -1
+					}
+
+				]
+			}
+		}
+	},
+	"metrics": {
+		"aggregation_dimensions": [
+			[
+				"InstanceId"
+			]
+		],
+		"append_dimensions": {
+			"AutoScalingGroupName": "${aws:AutoScalingGroupName}",
+			"ImageId": "${aws:ImageId}",
+			"InstanceId": "${aws:InstanceId}",
+			"InstanceType": "${aws:InstanceType}"
+		},
+		"metrics_collected": {
+			"cpu": {
+				"measurement": [
+					"cpu_usage_idle",
+					"cpu_usage_iowait",
+					"cpu_usage_user",
+					"cpu_usage_system"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				],
+				"totalcpu": false
+			},
+			"disk": {
+				"measurement": [
+					"used_percent",
+					"inodes_free"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				]
+			},
+			"diskio": {
+				"measurement": [
+					"io_time"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				]
+			},
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 30
+			},
+			"statsd": {
+				"metrics_aggregation_interval": 10,
+				"metrics_collection_interval": 30,
+				"service_address": ":8125"
+			},
+			"swap": {
+				"measurement": [
+					"swap_used_percent"
+				],
+				"metrics_collection_interval": 30
+			}
+		}
+	}
+}
+END
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:amazon-cloudwatch-agent.json
