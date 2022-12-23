46f2ed2cacdd8136508271bc65869e17f37d912b
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2022-12-23 14:31:47 +0530

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,5 +1,6 @@
+# security group for ASG
 resource "aws_security_group" "SG" {
-  name   = "test-sg"
+  name   = "asg-sg"
   vpc_id = module.vpc.vpc_id
 
   ingress {
@@ -25,23 +26,23 @@ resource "aws_security_group" "SG" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+   
   ingress {
-    description = "ALL"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    
-  ingress {
-    description      = "access"
+    description      = "access from pritunl"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     security_groups = [aws_security_group.pritunl-sg.id]
   }
-
+  
+  ingress {
+    description      = "access from alb"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+  
   egress {
     from_port   = 0
     to_port     = 0
@@ -50,7 +51,56 @@ resource "aws_security_group" "SG" {
   }
 
   tags = {
-    Name = "test-sg"
+    Name = "asg-sg"
   }
 }
-####
+
+  
+# security group for ALB
+resource "aws_security_group" "alb-sg" {
+  name   = "alb-sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
