eb7db3af2e8e9a68bc51b8a50132065470bac9fa
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2022-12-23 13:09:33 +0530

    final

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,9 +3,9 @@ resource "aws_instance" "s1" {
   instance_type          = "t3a.small"
   availability_zone      = "us-east-2a"
   key_name               = "adikp1"
-  security_groups        = [aws_security_group.SG.id]
+  security_groups        = [aws_security_group.pritunl-sg.id]
   subnet_id              = element(module.vpc.public_subnets, 0)
-  vpc_security_group_ids = [aws_security_group.SG.id]
+  vpc_security_group_ids = [aws_security_group.pritunl-sg.id]
 
   tags = {
     Name = "adi-test-public"
